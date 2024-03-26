878c493c4f46b414eeef620ffbd253a80dfd5467
KanterIV <126972573+KanterIV@users.noreply.github.com>
2024-03-26 10:12:06 +0100

    Added reset css, modern-mormalize, fonts, icons

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import "modern-normalize/modern-normalize.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
