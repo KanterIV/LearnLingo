1a206b864c155087c296f057e13b6ad3b3147ebd
KanterIV <KanterIV@users.noreply.github.com>
2024-06-21 17:58:28 +0000

    deploy: e297c2a5a790d03f299c7e2bcc58ae721c0ff132

--- assets/Favorite-CHemLUzR.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-gbRTbLNH.js";const e=()=>r.jsx("div",{children:"Favorite"});export{e as default};
---
+++ assets/Favorite-CoOttwts.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DFkVrxKu.js";const e=()=>r.jsx("div",{children:"Favorite"});export{e as default};
