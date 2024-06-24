2324b7c5d789834809cb4c1e53f37a0e9a4ce57e
KanterIV <126972573+KanterIV@users.noreply.github.com>
2024-06-24 10:55:28 +0200

    added markup to rhe favorite page

--- src/pages/Favorite/Favorite.jsx
+++ src/pages/Favorite/Favorite.jsx
@@ -1,5 +1,33 @@
+import { useSelector } from "react-redux";
+import { selectFavoriteteachers } from "../../redux/user/userSelectors";
+import TeachersList from "../../components/TeachersList/TeachersList";
+import { useState } from "react";
+import Button from "../../components/Button/Button";
+
 const Favorite = () => {
-  return <div>Favorite</div>;
+  const [visibleTeachersArr, setVisibleTeachersArr] = useState(4);
+  const favotireTeachersArr = useSelector(selectFavoriteteachers);
+
+  const onloadMoreBtnClick = () => {
+    setVisibleTeachersArr((prevState) => prevState + 4);
+  };
+
+  return (
+    <>
+      <TeachersList
+        teachersArr={favotireTeachersArr.slice(0, visibleTeachersArr)}
+      />
+      {favotireTeachersArr.length > visibleTeachersArr && (
+        <Button
+          styledClass="load-more-btn"
+          buttonType="button"
+          onClickFunction={onloadMoreBtnClick}
+        >
+          Load more
+        </Button>
+      )}
+    </>
+  );
 };
 
 export default Favorite;
