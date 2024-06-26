f17bd4b46811233480afa9c3a75c1a8d86f7e385
KanterIV <126972573+KanterIV@users.noreply.github.com>
2024-06-26 21:12:55 +0200

    implemented teacher filtering logic

--- src/pages/Teachers/Teachers.jsx
+++ src/pages/Teachers/Teachers.jsx
@@ -8,12 +8,13 @@ import FilterSelect from "../../components/FilterSelect/FilterSelect";
 
 const Teachers = () => {
   const [visibleTeachersArr, setVisibleTeachersArr] = useState(4);
+  const [filtredArray, setFiltredArray] = useState([]);
   const [teacherfilters, setTeacherFilters] = useState({
     languages: null,
     levels: null,
     price_per_hour: null,
   });
-  console.log(teacherfilters);
+  console.log(filtredArray);
 
   const allTeachersArr = useSelector(selectAllteachers);
   const dispatch = useDispatch();
@@ -25,11 +26,53 @@ const Teachers = () => {
     dispatch(getAllTeachers());
   }, [dispatch]);
 
+  useEffect(() => {
+    const handleFilter = (teachers, filters) => {
+      const { languages, levels, price_per_hour } = filters;
+      if (!teachers) {
+        return [];
+      }
+      if (!languages && !levels && !price_per_hour) {
+        return [];
+      }
+
+      const filteredTeacherArray = teachers.filter((teacher) => {
+        let matches = true;
+
+        if (teacherfilters.languages) {
+          matches = matches && teacher.languages.includes(filters.languages);
+        }
+
+        if (teacherfilters.levels) {
+          matches = matches && teacher.levels.includes(filters.levels);
+        }
+
+        if (teacherfilters.price_per_hour) {
+          matches =
+            matches &&
+            Number(teacher.price_per_hour) === Number(filters.price_per_hour);
+        }
+        return matches;
+      });
+
+      if (filteredTeacherArray.length > 0) {
+        return filteredTeacherArray;
+      } else {
+        return [];
+      }
+    };
+    const filtered = handleFilter(allTeachersArr, teacherfilters);
+    setFiltredArray(filtered);
+  }, [allTeachersArr, teacherfilters]);
+
+  const teachersToShow =
+    filtredArray.length > 0 ? filtredArray : allTeachersArr;
+
   return (
     <>
       <FilterSelect setTeacherFilters={setTeacherFilters} />
-      <TeachersList teachersArr={allTeachersArr.slice(0, visibleTeachersArr)} />
-      {allTeachersArr.length > visibleTeachersArr && (
+      <TeachersList teachersArr={teachersToShow.slice(0, visibleTeachersArr)} />
+      {teachersToShow.length > visibleTeachersArr && (
         <Button
           styledClass="registerBtn"
           buttonType="button"
