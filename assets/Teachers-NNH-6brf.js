6b7780131a58b4a5f047d28aed86139103763257
KanterIV <KanterIV@users.noreply.github.com>
2024-07-07 18:28:38 +0000

    deploy: 1866990dd3387f19dce7a4387bb5538f6cb7a6aa

---
+++ assets/Teachers-NNH-6brf.js
@@ -0,0 +1,6 @@
+import{u as p,r as e,b as g,s as x,c as T,g as f,j as s,B as m}from"./index-Du810eOa.js";import{h as A,F as b,T as j}from"./teacherFiltration-CBObKbXw.js";const S=p.section`
+  background-color: var(--secondary-white);
+
+  padding-top: 16px;
+  padding-bottom: 96px;
+`,v=()=>{const[a,i]=e.useState(4),[c,h]=e.useState([]),[l,u]=e.useState({languages:null,levels:null,price_per_hour:null}),t=g(x),o=T(),d=()=>{i(r=>r+4)};e.useEffect(()=>{o(f())},[o]),e.useEffect(()=>{const r=A(t,l);h(r)},[t,l]);const n=c.length>0?c:t;return s.jsx(S,{children:s.jsxs("div",{className:"container teachers-container",children:[s.jsx(b,{setTeacherFilters:u}),s.jsx(j,{teachersArr:n.slice(0,a)}),n.length>a&&s.jsx(m,{styledClass:"registerBtn",buttonType:"button",onClickFunction:d,children:"Load more"})]})})};export{v as default};
