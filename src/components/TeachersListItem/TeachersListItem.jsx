c57d092dbd0722ed14cb32078c1fc5cab74300c9
KanterIV <126972573+KanterIV@users.noreply.github.com>
2024-07-09 21:12:41 +0200

    added all styles for teachers page
    Styles for Teachers page tablet\desktop version

--- src/components/TeachersListItem/TeachersListItem.jsx
+++ src/components/TeachersListItem/TeachersListItem.jsx
@@ -18,6 +18,7 @@ import BookIcon from "../../assets/icons/book.svg?react";
 import StartIcon from "../../assets/icons/star.svg?react";
 import HeartIcon from "../../assets/icons/heart.svg?react";
 import OnlineIcon from "../../assets/icons/online.svg?react";
+import { useMediaQuery } from "react-responsive";
 
 const TeachersListItem = ({
   id,
@@ -39,6 +40,8 @@ const TeachersListItem = ({
   const [isFullDetails, setIsFullDetails] = useState(false);
   const favoriteTeachersArr = useSelector(selectFavoriteteachers);
   const authenticated = useSelector(selectUserSingnedIn);
+  const isTablet = useMediaQuery({ query: "(min-width: 320px)" });
+
   const teacherData = {
     id,
     name,
@@ -81,86 +84,95 @@ const TeachersListItem = ({
 
   return (
     <StyledTeachersListItem>
-      <div className="teacher-avatar-wrapper">
-        <img
-          src={avatar_url}
-          width="96px"
-          className="teacher-avatar"
-          alt="Teacher avatar"
-        />
-        <OnlineIcon className="online-icon" />
-      </div>
-      <div className="teacher-name-wrapper">
-        <p className="teachers-category">Languages</p>
-        <h2 className="teacher-name">{`${name} ${surname}`}</h2>
-      </div>
-      <ul className="stats-list">
-        <li className="stats-list-item">
-          <BookIcon />
-          <p className="stats-text-content">Lessons online</p>
-        </li>
-        <li className="stats-list-item">
-          <p className="stats-text-content">{`Lessons done: ${lessons_done}`}</p>
-        </li>
-        <li className="stats-list-item">
-          <StartIcon />
-          <p className="stats-text-content">{`Rating: ${rating}`}</p>
-        </li>
-        <li className="stats-list-item">
-          <p className="stats-text-content">
-            Price / 1 hour:
-            <span className="stats-price">{` ${price_per_hour}$`}</span>
-          </p>
-        </li>
-      </ul>
-      <Button
-        styledClass={
-          authenticated && isFavorite
-            ? "icon-heart-btn isFavorite"
-            : "icon-heart-btn"
-        }
-        buttonType="button"
-        tooltipAnchor="unlogged"
-        onClickFunction={toggleFavoriteTeacher}
-      >
-        <HeartIcon />
-      </Button>
-      <p className="teacher-info">
-        <span className="teacher-info-subtitle">Speaks: </span>
-        <span className="teacher-speaks">{`${languages.join(", ")}`}</span>
-      </p>
-      <p className="teacher-info">
-        <span className="teacher-info-subtitle">Lesson Info:</span>
-        {` ${lesson_info}`}
-      </p>
-      <p className="teacher-info conditions">
-        <span className="teacher-info-subtitle">Conditions:</span>
-        {` ${conditions.join(" ")}`}
-      </p>
-
-      {isFullDetails && (
-        <div className="rest-teacher-info">
-          <p className="teacher-experience">{experience}</p>
-          <ul className="reviews-list">
-            <Reviews reviews={reviews} />
-          </ul>
+      <div className="left-side-wrapper">
+        <div className="teacher-avatar-wrapper">
+          <img
+            src={avatar_url}
+            width="96px"
+            className="teacher-avatar"
+            alt="Teacher avatar"
+          />
+          <OnlineIcon className="online-icon" />
         </div>
-      )}
-      <Button
-        styledClass="readMoreBtn"
-        buttonType="button"
-        onClickFunction={onReadMoreBtnClick}
-      >
-        {isFullDetails ? "Hide details" : "Read more"}
-      </Button>
-      <TeacherLevelsList levels={levels} />
-      <Button
-        styledClass="bookBtn"
-        buttonType="button"
-        onClickFunction={onBookingBtnClick}
-      >
-        Book trial lesson
-      </Button>
+      </div>
+      <div className="right-side-wrapper">
+        <div className="common-info-wrapper">
+          <div className="teacher-name-wrapper">
+            <p className="teachers-category">Languages</p>
+            {isTablet && (
+              <h2 className="teacher-name">{`${name} ${surname}`}</h2>
+            )}
+          </div>
+          <ul className="stats-list">
+            <li className="stats-list-item">
+              <BookIcon />
+              <p className="stats-text-content">Lessons online</p>
+            </li>
+            <li className="stats-list-item">
+              <p className="stats-text-content">{`Lessons done: ${lessons_done}`}</p>
+            </li>
+            <li className="stats-list-item">
+              <StartIcon />
+              <p className="stats-text-content">{`Rating: ${rating}`}</p>
+            </li>
+            <li className="stats-list-item">
+              <p className="stats-text-content">
+                Price / 1 hour:
+                <span className="stats-price">{` ${price_per_hour}$`}</span>
+              </p>
+            </li>
+          </ul>
+          <Button
+            styledClass={
+              authenticated && isFavorite
+                ? "icon-heart-btn isFavorite"
+                : "icon-heart-btn"
+            }
+            buttonType="button"
+            tooltipAnchor="unlogged"
+            onClickFunction={toggleFavoriteTeacher}
+          >
+            <HeartIcon />
+          </Button>
+        </div>
+        {/* {isTablet && <h2 className="teacher-name">{`${name} ${surname}`}</h2>} */}
+        <p className="teacher-info">
+          <span className="teacher-info-subtitle">Speaks: </span>
+          <span className="teacher-speaks">{`${languages.join(", ")}`}</span>
+        </p>
+        <p className="teacher-info">
+          <span className="teacher-info-subtitle">Lesson Info:</span>
+          {` ${lesson_info}`}
+        </p>
+        <p className="teacher-info conditions">
+          <span className="teacher-info-subtitle">Conditions:</span>
+          {` ${conditions.join(" ")}`}
+        </p>
+
+        {isFullDetails && (
+          <div className="rest-teacher-info">
+            <p className="teacher-experience">{experience}</p>
+            <ul className="reviews-list">
+              <Reviews reviews={reviews} />
+            </ul>
+          </div>
+        )}
+        <Button
+          styledClass="readMoreBtn"
+          buttonType="button"
+          onClickFunction={onReadMoreBtnClick}
+        >
+          {isFullDetails ? "Hide details" : "Read more"}
+        </Button>
+        <TeacherLevelsList levels={levels} />
+        <Button
+          styledClass="bookBtn"
+          buttonType="button"
+          onClickFunction={onBookingBtnClick}
+        >
+          Book trial lesson
+        </Button>
+      </div>
     </StyledTeachersListItem>
   );
 };
--- src/components/TeachersListItem/TeachersListItem.styled.js
+++ src/components/TeachersListItem/TeachersListItem.styled.js
@@ -4,65 +4,139 @@ export const StyledTeachersListItem = styled.li`
   padding: 24px;
   border-radius: 24px;
   background-color: var(--primary-white);
+
   @media screen and (max-width: 767px) {
     position: relative;
   }
 
+  @media screen and (min-width: 768px) {
+    display: flex;
+    align-items: flex-start;
+    gap: 48px;
+  }
+
+  .left-side-wrapper {
+    @media screen and (max-width: 767px) {
+      margin-bottom: 24px;
+    }
+  }
+
+  .common-info-wrapper {
+    margin-bottom: 32px;
+
+    @media screen and (min-width: 768px) {
+      display: flex;
+      align-items: flex-start;
+    }
+  }
+
   .teacher-avatar-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     position: relative;
-    width: 160px;
-    height: 160px;
-    margin: 0 auto;
+    width: 190px;
+    height: 190px;
     z-index: var(--main-z-index);
     border-radius: 50%;
 
     @media screen and (max-width: 767px) {
-      margin-bottom: 30px;
+      margin: 0 auto;
+    }
+
+    @media screen and (min-width: 768px) {
+      width: 120px;
+      height: 120px;
     }
 
     & svg {
       width: 16px;
       height: 16px;
       position: absolute;
-      top: 12px;
-      right: 21px;
+      top: 26px;
+      right: 36px;
+
+      @media screen and (min-width: 768px) {
+        width: 12px;
+        height: 12px;
+        top: 19px;
+        right: 21px;
+      }
     }
   }
 
   .teacher-avatar-wrapper::before {
     content: "";
     position: absolute;
-    width: 180px;
-    height: 180px;
+    width: 190px;
+    height: 190px;
     border-radius: 50%;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     z-index: var(--lowest-z-index);
     border: 3px solid var(--accent-color);
+
+    @media screen and (min-width: 768px) {
+      width: 120px;
+      height: 120px;
+    }
   }
 
   .teacher-avatar {
-    width: 100%;
+    width: 160px;
+    height: 160px;
     border-radius: 50%;
+
+    @media screen and (min-width: 768px) {
+      width: 96px;
+      height: 96px;
+    }
+  }
+
+  .right-side-wrapper {
+    @media screen and (min-width: 1440px) {
+      width: 100%;
+    }
   }
 
   .teacher-name-wrapper {
-    margin-bottom: 16px;
+    @media screen and (max-width: 767px) {
+      margin-bottom: 16px;
+    }
   }
+
   .teachers-category {
     font-weight: 500;
     line-height: 1.5;
     color: var(--primary-grey);
     text-align: center;
     margin-bottom: 6px;
+
+    @media screen and (min-width: 768px) {
+      margin-bottom: 8px;
+      text-align: start;
+    }
   }
 
   .teacher-name {
+    width: 100%;
+    margin: 0 auto;
     font-weight: 500;
     font-size: 24px;
     line-height: 1;
     text-align: center;
+
+    @media screen and (min-width: 768px) {
+      width: 150px;
+      text-align: start;
+      font-size: 20px;
+    }
+
+    @media screen and (min-width: 1440px) {
+      font-size: 24px;
+      width: 271px;
+    }
   }
 
   .stats-list {
@@ -71,7 +145,16 @@ export const StyledTeachersListItem = styled.li`
     justify-content: center;
     align-items: center;
     gap: 16px;
-    margin-bottom: 32px;
+
+    @media screen and (min-width: 768px) {
+      column-gap: 16px;
+      row-gap: 8px;
+    }
+
+    @media screen and (min-width: 1440px) {
+      gap: 32px;
+      margin-right: auto;
+    }
   }
 
   .stats-list-item:nth-child(2n + 1) {
@@ -89,10 +172,13 @@ export const StyledTeachersListItem = styled.li`
     color: var(--green-price);
   }
   .icon-heart-btn {
-    position: absolute;
-    top: 0;
-    right: 0;
-    transform: translate(-100%, 100%);
+    @media screen and (max-width: 767px) {
+      position: absolute;
+      top: 0;
+      right: 0;
+      transform: translate(-100%, 100%);
+    }
+
     border: none;
     background-color: transparent;
 
@@ -102,6 +188,11 @@ export const StyledTeachersListItem = styled.li`
       }
       width: 20px;
       height: 20px;
+
+      @media screen and (min-width: 1440px) {
+        width: 26px;
+        height: 26px;
+      }
     }
 
     &.isFavorite {
