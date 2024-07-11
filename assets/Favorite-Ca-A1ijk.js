a03fcb70035c94c496f9fd9e9d8f8be914fcf24b
KanterIV <KanterIV@users.noreply.github.com>
2024-07-11 19:53:30 +0000

    deploy: 78ea770082c6854a497a2a93406767aba0d9aecb

--- assets/Favorite-Ca-A1ijk.js
+++ assets/Favorite-Ca-A1ijk.js
@@ -1,4 +1,4 @@
-import{u as h,r as t,b as p,d as x,j as e,B as f}from"./index-D64_p6f6.js";import{h as v,F,T as g}from"./teacherFiltration-BMolE5Px.js";const m=h.section`
+import{u as h,r as t,b as p,d as x,j as e,B as f}from"./index-DN3y96ux.js";import{h as v,F,T as g}from"./teacherFiltration-ZPGXlbqW.js";const m=h.section`
   background-color: var(--secondary-white);
 
   padding-top: 16px;
